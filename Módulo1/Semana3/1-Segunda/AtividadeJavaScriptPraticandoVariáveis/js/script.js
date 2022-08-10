2fa0e685b9cc4c7a5ebaa0a037a4986d64efccdc
Lelouching <engenheirogamer123@gmail.com>
2022-08-10 12:24:30 -0300

    Atualizações e atividades

---
+++ Módulo1/Semana3/1-Segunda/AtividadeJavaScriptPraticandoVariáveis/js/script.js
@@ -0,0 +1,51 @@
+let nome = "Jhonata";
+let sobrenome = "Cardoso"
+let idade = 18;
+console.log(`Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos!`);
+
+let preco1 = 50;
+let preco2 = 100;
+let precoTotal = preco1 * preco2;
+console.log(`Peguei o ${preco1} e multipliquei pelo ${preco2} e obtive o resultado de ${precoTotal}.`);
+
+let numero1 = 33;
+let numero2 = 48;
+let somaNumero = numero1 + numero2;
+console.log(`A soma do ${numero1} com o ${numero2} é de ${somaNumero}`);
+
+let contador = 0;
+let soma1 = 1;
+let soma2 = 1;
+let soma3 = 1;
+let soma4 = 1;
+let soma5 = 1;
+console.log(`${contador + soma1}`);
+
+contador = 1;
+console.log(`${contador + soma2}`);
+
+contador = 2;
+console.log(`${contador + soma3}`);
+
+contador = 3;
+console.log(`${contador + soma4}`);
+
+contador = 4;
+console.log(`E o valor do contador chegou à: ${contador + soma5}`);
+
+let anoAtual = 2022;
+let anoNascimento = 2003;
+let idadeAtual = anoAtual - anoNascimento;
+console.log(`No ${anoAtual} eu vou ter ${idadeAtual} anos.`);
+
+let diaAtual = "10/";
+let mesAtual = "08/";
+anoAtual = "2022";
+let dataAtualFormado = diaAtual+mesAtual+anoAtual;
+console.log(`Hoje é: ${dataAtualFormado}`)
+
+let valorA = 3;
+let valorB = 7;
+valorA = 7;
+valorB = 3;
+console.log(`Antes o valorA era 3 e agora é ${valorA}`)
